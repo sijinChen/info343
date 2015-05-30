e532557d373e02d6cc5d1f0ccb3a5f8450e7b653
mkfreeman <michael.k.freeman@gmail.com>
2015-05-30 13:15:53 -0700

    Reorganized to single page architecture

--- js/app.js
+++ js/app.js
@@ -1,23 +1,81 @@
-// Instantiate app
+// Main app
+var mainApp = angular.module('MainApp', ['LandingApp', 'LectureApp', 'ChallengeApp'])
+
+// main Controller
+mainApp.controller('MainController', ['$scope', function($scope){
+  $scope.page = 'pages/landing.html'
+  $scope.setView = function(url) {
+    $scope.page = 'pages/' + url
+  }
+}])
+
+// Landing page appp
 var landingModule = angular.module('LandingApp',[])
 
-// Get data
-landingModule.factory('Items', ['$http', function($http){
+// Landing page data
+landingModule.factory('LandingData', ['$http', function($http){
   var Url   = "data/content.csv";
-  var Items = $http.get(Url).then(function(response){
+  var LandingData = $http.get(Url).then(function(response){
      arr = CSVToArray(response.data);
      var ret = {}
      arr.map(function(d) {ret[d.section] = d.content})
      return ret
   })
-  return Items
+  return LandingData
+}]);
+
+// Landing page controller
+landingModule.controller('LandingController',['$scope', 'LandingData', function($scope, LandingData){
+	LandingData.then(function(data){
+    console.log("landing content ", data)
+	  $scope.content = data;
+	});
+}])
+    
+
+// Instantiate app
+var lectureModule = angular.module('LectureApp',[])
+
+// Get data
+lectureModule.factory('Items', ['$http', function($http){
+  var Url   = "data/lectures.csv";
+  var Items = $http.get(Url).then(function(response){
+    test = response.data
+     return CSVToArray(response.data);
+  });
+  return Items;
+}]);
+
+// Lecture controller
+lectureModule.controller('LectureController',['$scope', 'Items', function($scope, Items){
+  Items.then(function(data){
+    $scope.items = data;
+  });
+}])
+
+
+// Challenge App
+var challengeModule = angular.module('ChallengeApp',[])
+
+// Challenge data
+challengeModule.factory('ChallengeData', ['$http', function($http){
+  var Url   = "data/challenges.csv";
+  var ChallengeData = $http.get(Url).then(function(response){
+     return CSVToArray(response.data);
+  });
+  return ChallengeData;
 }]);
 var dat;
 // Controller
-landingModule.controller('LandingController',['$scope', 'Items', function($scope, Items){
-	Items.then(function(data){
-    dat = data
-	  $scope.content = data;
-	});
+challengeModule.controller('ChallengeController',['$scope', 'ChallengeData', function($scope, ChallengeData){
+  ChallengeData.then(function(data){
+    $scope.challenges = data;
+    $scope.currentChallenge = null
+     dat = data
+    $scope.viewChallenge = function(url) {
+      var currentChallenge = url == null ? null : 'challenges/' + url + '.html'
+      $scope.currentChallenge = currentChallenge
+    }
+  });
 }])
     

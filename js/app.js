7bb227f5d6fdc7fa110a8ce62ecbca5f3dfb84fd
Michael Freeman <michael.k.freeman@gmail.com>
2015-06-11 13:54:21 -0700

    Got routes working properly

--- js/app.js
+++ js/app.js
@@ -1,41 +1,54 @@
-// Main app
-var mainApp = angular.module('MainApp', ['LandingApp', 'LectureApp', 'ChallengeApp','ngRoute'])
+// Start ap
+var mainApp = angular.module('MainApp', ['ngRoute'])
 
-
-.controller('Test', function($scope, $routeParams) {
-     $scope.name = "TestController";
-     $scope.params = $routeParams;
- })
-// main Controller
-.controller('MainController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- })
-.config(function($routeProvider, $locationProvider) {
+// Config route provider
+.config(function($routeProvider) {
   $routeProvider
-   .when('/test/', {
+   .when('/challenges/:challenge_id?', {
     templateUrl: 'pages/challenges.html',
     controller: 'ChallengeController',
   })
-   $locationProvider.html5Mode(true);
+   .when('/lectures/', {
+    templateUrl: 'pages/lectures.html',
+    controller: 'LectureController',
+  })
+   .when('/', {
+    templateUrl: 'pages/landing.html',
+    controller: 'LandingController',
+  })
 })
-mainApp.controller('MainController', ['$scope', '$location', function($scope,$location){
-  var location = $location.path().split('/')[1] == undefined ? '' : $location.path().split('/')[1]
-  $scope.page = location.length == 0 ? 'pages/landing.html' : 'pages/' + location + '.html'
-  $scope.setView = function(url) {
-    console.log('url ', url)
-    var path = url.indexOf("landing") == -1 ? url.replace('.html','') : ''
-    $location.path(path)  
-    $scope.page = 'pages/' + url
-  }
+
+// Landing page controller
+.controller('LandingController',['$scope', 'LandingData', function($scope, LandingData){
+  LandingData.then(function(data){
+    $scope.content = data;
+  });
 }])
 
-// Landing page appp
-var landingModule = angular.module('LandingApp',[])
+// Challenge Controller
+.controller('ChallengeController', function($scope, $q, $routeParams, ChallengeData, ChallengeRubric){
+  $q.all([ChallengeData, ChallengeRubric]).then(function(values){    
+    $scope.challenges = values[0];    
+    $scope.rubrics = {}
+    values[1].map(function(d) {
+      if($scope.rubrics[d.challenge_id] == undefined)$scope.rubrics[d.challenge_id] = []
+      $scope.rubrics[d.challenge_id].push(d)
+    })
+    $scope.currentChallenge = $routeParams.challenge_id
+    $scope.currentRubric = $scope.rubrics[$routeParams.challenge_id]
+  })
+})
+
+// Lecture controller
+.controller('LectureController',['$scope', 'Items', function($scope, Items){
+  Items.then(function(data){
+    $scope.items = data;
+  });
+}])
+
 
 // Landing page data
-landingModule.factory('LandingData', ['$http', function($http){
+.factory('LandingData', ['$http', function($http){
   var Url   = "data/content.csv";
   var LandingData = $http.get(Url).then(function(response){
      arr = CSVToArray(response.data);
@@ -44,84 +57,34 @@ landingModule.factory('LandingData', ['$http', function($http){
      return ret
   })
   return LandingData
-}]);
-
-// Landing page controller
-landingModule.controller('LandingController',['$scope', 'LandingData', function($scope, LandingData){
-	LandingData.then(function(data){
-	  $scope.content = data;
-	});
 }])
     
 
-// Instantiate app
-var lectureModule = angular.module('LectureApp',[])
-
 // Get data
-lectureModule.factory('Items', ['$http', function($http){
+.factory('Items', ['$http', function($http){
   var Url   = "data/lectures.csv";
   var Items = $http.get(Url).then(function(response){
     test = response.data
      return CSVToArray(response.data);
   });
   return Items;
-}]);
-
-// Lecture controller
-lectureModule.controller('LectureController',['$scope', 'Items', function($scope, Items){
-  Items.then(function(data){
-    $scope.items = data;
-  });
 }])
 
 
-// Challenge App
-var challengeModule = angular.module('ChallengeApp',[])
-
 // Challenge data
-challengeModule.factory('ChallengeData', ['$http', function($http){
+.factory('ChallengeData', ['$http', function($http){
   var Url   = "data/challenges.csv";
   var ChallengeData = $http.get(Url).then(function(response){
      return CSVToArray(response.data);
   });
   return ChallengeData;
-}]);
+}])
 
 // Challenge data
-challengeModule.factory('ChallengeRubric', ['$http', function($http){
-  console.log('get rubrics!')
+.factory('ChallengeRubric', ['$http', function($http){
   var Url   = "data/rubrics.csv";
   var ChallengeRubrics = $http.get(Url).then(function(response){
      return CSVToArray(response.data);
   });
   return ChallengeRubrics
 }]);
-
-// Controller
-challengeModule.controller('ChallengeController',['$scope', '$location', '$q', 'ChallengeData', 'ChallengeRubric', function($scope, $location, $q, ChallengeData, ChallengeRubric){
-  $q.all([ChallengeData, ChallengeRubric]).then(function(values){    
-    $scope.challenges = values[0];    
-    $scope.rubrics = values[1];
-    $scope.getChallenge = function() {
-      var challenge = $location.path().split('/')[2] == undefined ? null : $location.path().split('/')[2] 
-      console.log('challenge ', challenge)
-      $scope.currentChallenge = challenge 
-      $scope.currentChallengeUrl = challenge == undefined ? null : 'challenges/' + challenge +'.html'
-      $scope.currentRubric = $scope.rubrics.filter(function(d) {
-        if($scope.currentChallenge == null) return false
-        console.log(d.challenge_id, $scope.currentChallenge)
-        return d.challenge_id == $scope.currentChallenge
-      });  
-      $scope.submitUrl = $scope.currentChallenge == null ? '' : $scope.challenges.filter(function(d){return d.challenge_id == $scope.currentChallenge})[0].submitUrl
-      console.log('submitURl, ',  $scope.submitUrl)
-    }
-    $scope.getChallenge()
-    $scope.viewChallenge = function(url) {
-      var path = url == null ? 'challenges': $location.path() +'/' + url
-      $location.path(path)
-      $scope.getChallenge()
-    }
-
-  })
-}])
-    

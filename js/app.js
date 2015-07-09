3588f30a4296ca765044be0cb90a7a2bcab04993
SIJIN CHEN <AmyChen@SIJINs-MacBook-Pro.local>
2015-07-09 11:40:02 -0700

    draft

--- js/app.js
+++ js/app.js
@@ -1,105 +1,64 @@
-// Start app
-var mainApp = angular.module('MainApp', ['ngRoute'])
+var dataSong;
+var dataArtist;
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var myApp = angular.module('myApp', [])
+var volumeCurrent = 0.5;
 
-// Config route provider
-.config(function($routeProvider) {
-  $routeProvider
-   .when('/challenges/:challenge_id?', {
-    templateUrl: 'pages/challenges.html',
-    controller: 'ChallengeController',
-  })
-   .when('/lectures/', {
-    templateUrl: 'pages/lectures.html',
-    controller: 'LectureController',
-  })
-   .when('/', {
-    templateUrl: 'pages/landing.html',
-    controller: 'LandingController',
-  })
+$("#card").flip({
+    axis: 'x',
+    trigger: 'hover'
+});
+
+
+
+var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
+    $scope.audioObject = {}
+    $scope.order = 'name'
+    $scope.getSongs = function() {
+        $http.get(baseUrl + $scope.track).success(function(response){
+            dataSong = $scope.tracks = response.tracks.items
+
+        })
+    }
+    $scope.getArtists = function(val) {
+        $http.get(baseUrl + $scope.track).success(function(response){
+            if(response.tracks.items.artists[0].name = val){
+                dataArtist = $scope.artistAlbum = response.tracks.items.name
+            }
+        })
+        return dataArtist;
+    }
+
+    $scope.play = function(song) {
+
+        if($scope.currentSong == song) {
+            $scope.audioObject.pause()
+            $scope.currentSong = false
+            $('#setVolume').hide();
+            return
+        }
+        else {
+            if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+            $scope.audioObject = new Audio(song);
+            $scope.audioObject.volume = volumeCurrent;
+            $scope.audioObject.play()
+            $scope.currentSong = song
+
+            function setVolume(vol) {
+                volumeCurrent = vol;
+                $scope.audioObject.volume = vol;
+            }
+
+            window.Player = {
+                setVolume: setVolume,
+            };
+        }
+
+        $("#setVolume").show();
+    }
+    $scope.setOrder = function(value){
+        $scope.order = value;
+    }
 })
 
-// Landing page controller
-.controller('LandingController',['$scope', 'LandingData', function($scope, LandingData){
-  LandingData.then(function(data){
-    $scope.content = data;
-  });
-}])
 
-// Challenge Controller
-.controller('ChallengeController', function($scope, $q, $routeParams, ChallengeData, ChallengeRubric){
-  $q.all([ChallengeData, ChallengeRubric]).then(function(values){    
-    $scope.challenges = values[0];    
-    $scope.rubrics = {}
-    values[1].map(function(d) {
-      if($scope.rubrics[d.challenge_id] == undefined)$scope.rubrics[d.challenge_id] = []
-      $scope.rubrics[d.challenge_id].push(d)
-    })
-    $scope.currentChallenge = $routeParams.challenge_id
-    $scope.currentRubric = $scope.rubrics[$routeParams.challenge_id]
-    $scope.submitUrl = $scope.challenges.filter(function(d) { return d.challenge_id == $scope.currentChallenge})[0].submitUrl
-  })
-})
-
-// Lecture controller
-.controller('LectureController',['$scope', 'Items', function($scope, Items){
-  Items.then(function(data){
-    $scope.items = data;
-  });
-}])
-
-// Schedule controller 
-.controller('ScheduleController', function($scope, $q, ChallengeData, Items){
-  $q.all([ChallengeData, Items]).then(function(values){    
-    $scope.challenges = values[0];    
-    $scope.lectures = values[1];
-    $scope.challenges.map(function(challenge){
-      var lecture = $scope.lectures.filter(function(lecture){
-        return lecture.date == challenge.due
-      })[0]
-      if(lecture == undefined) return
-      lecture.due = challenge.title
-      lecture.challengeUrl = challenge.challenge_id
-    })
-  })
-})
-// Landing page data
-.factory('LandingData', ['$http', function($http){
-  var Url   = "data/content.csv";
-  var LandingData = $http.get(Url).then(function(response){
-     arr = CSVToArray(response.data);
-     var ret = {}
-     arr.map(function(d) {ret[d.section] = d.content})
-     return ret
-  })
-  return LandingData
-}])
-    
-
-// Get data
-.factory('Items', ['$http', function($http){
-  var Url   = "data/lectures.csv";
-  var Items = $http.get(Url).then(function(response){
-    test = response.data
-     return CSVToArray(response.data);
-  });
-  return Items;
-}])
-
-
-// Challenge data
-.factory('ChallengeData', ['$http', function($http){
-  var Url   = "data/challenges.csv";
-  var ChallengeData = $http.get(Url).then(function(response){
-     return CSVToArray(response.data);
-  });
-  return ChallengeData;
-}])
-
-// Challenge data
-.factory('ChallengeRubric', ['$http', function($http){
-  var Url   = "data/rubrics.csv";
-  var ChallengeRubrics = $http.get(Url).then(function(response){
-     return CSVToArray(response.data);
-  });
-  return ChallengeRubrics
-}]);
